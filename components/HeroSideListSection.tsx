0e8577b85aa9ed2408273c60cda14314d3010a80
Newsstate <newsstate6294@gmail.com>
2026-01-01 01:05:42 +0530

    Update HeroSideListSection.tsx

--- components/HeroSideListSection.tsx
+++ components/HeroSideListSection.tsx
@@ -2,7 +2,7 @@
 import { ArticleCard, WPPost } from "@/components/ArticleCard";
 
 // --- Configuration ---
-const API_URL = "https://newsstate24.com/wp-json/wp/v2";
+const API_URL = "https://khabar24live.com/wp-json/wp/v2";
 const POSTS_TO_FETCH = 6; // 1 Hero + 5 List items
 
 // --- Fetch Utility ---
